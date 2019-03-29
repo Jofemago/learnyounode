6090adeadd1be1c2a828a4a9dfa0a4dff77fa9ab
Jofemago <jofemago@utp.edu.co>
2019-03-28 22:43:20 -0500

    tercer ejercicio

---
+++ myfirstIO.js
@@ -0,0 +1,10 @@
+var process = require("process");
+var fs = require("fs");
+
+//console.log(process.argv);
+
+var buffer = fs.readFileSync(process.argv[2]); //representacion del archivo en un buffer, nombre de archivo por linea de comandos
+var data = buffer.toString(); //volver contenido en string
+var dataLines = data.split("\n"); //crea un arreglo separado por los saltos de linea
+
+console.log(dataLines.length); //cuantos elementos existan aqui es el numero
